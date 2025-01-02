b85942d58bb6d1d7daea03bf0193c4663820f9a9
Mina Shumaly <114043184+MinaShum@users.noreply.github.com>
2025-01-02 16:51:22 -0600

    Update VB.py

--- HorseDataset/VB.py
+++ HorseDataset/VB.py
@@ -1,3 +1,4 @@
+
 # Variational Bayes #
 
 import os
@@ -11,15 +12,12 @@ output_directory = r"/O11"
 directory_path = r"/s1"
 
 file_names = os.listdir(directory_path)
-
 file_paths = []
 for file_name in file_names:
     file_path = os.path.join(directory_path, file_name)
     file_paths.append(file_path)
-
 first = file_paths[:500]
 second = file_paths[500:]
-
 model_dir = r'/data'
 stan_file = os.path.join(model_dir, 'Model.stan')
 model = CmdStanModel(stan_file=stan_file)
@@ -31,25 +29,23 @@ for iteration in range(len(second)):
     except RuntimeError as e:
         print(f"Error occurred in iteration {iteration + 1}: {e}")
         continue  # Skip to the next iteration
-
     a = vi.stan_variable("s2_e")
     b = vi.stan_variable("s2_h")
     c = vi.stan_variable("trt_eff")
-
     c1, c2 = c
-
+    
     # Saving a, b, and c to a DataFrame
     df = pd.DataFrame({'a': [a], 'b': [b], 'c1': [c1], 'c2': [c2]})
-
+    
     # Constructing file path for DataFrame
     file_name = f"iteration_{iteration + 1}.csv"
     file_path = os.path.join(output_directory, file_name)
-
+    
     # Saving DataFrame to CSV
     df.to_csv(file_path, index=False)
     d = vi.stan_variable("horse_eff")
     e = vi.stan_variable("horse_probs")
-
+    
     # Constructing file paths for d and e arrays
     d_file_path = os.path.join(output_directory, f"horse_eff_{iteration + 1}.csv")
     e_file_path = os.path.join(output_directory, f"horse_probs_{iteration + 1}.csv")
